bbed8c1c1a88ce3e185976b851fd2104138e80a6
Spencer Thayer <me@spencerthayer.com>
2015-07-16 22:22:37 -0500

    .

--- deletemp3s.sh
+++ deletemp3s.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # Loop through unrelated files (*.txt, directories, etc)
-echo "THE FUN KILLER!"
 for f in ~/*; do
     f="${f%.*}"
     if [[ -f ${f}.mp3 ]]; then
         rm -f "${f}.mp3" && echo >&2 "${f}.mp3 deleted"
     fi
 done
+#find ~/ -type f -regextype posix-extended -iregex '.*\.(avi|mkv|wmv|mp4|mp5|flv|M4V|mpeg|mov|m1v|m2v|3gp|avchd)$' -delete
+#
+#for ext in avi mkv wmv mp4 mp5 flv M4V mpeg mov m1v m2v 3gp avchd; do
+#  rm -f ~/*.$ext;
+#done
