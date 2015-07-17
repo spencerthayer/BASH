5ca7eee6088c8c8b8ec68b3fa73b7941aa0db62e
Spencer Thayer <me@spencerthayer.com>
2015-07-17 15:53:30 -0500

    .

--- cleanMedia.sh
+++ cleanMedia.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 find ~/ -type f -regextype posix-extended -iregex '.*\.(wav|wave|mp2|mp3|mp4|mp4a|mp5|m4p|m4b|mpga|3ga|3gp|flac|cda|rec|ra|ram|rmx|rmj|rmm|m4a|m4v|wma|wmv|au|aiff|ogg|oga|midi|d00|avi|mkv|wmv|flv|M4V|mpeg|mov|m1v|m2v|avchd)$' -delete
+done
 # OPTION 2 DO NOT USE
 # for ext in wav wave mp2 mp3 mp4 mp4a mp5 m4p m4b mpga 3ga 3gp flac cda rec ra ram rmx rmj rmm m4a m4v wma wmv au aiff ogg oga midi d00 avi mkv wmv flv M4V mpeg mov m1v m2v avchd; do
 #  rm -f ~/*.$ext;
