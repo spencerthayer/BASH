747fb458b91bb3c73ce3cf0a1235822c62dce5e0
Spencer Thayer <me@spencerthayer.com>
2015-07-17 16:12:46 -0500

    .

--- addWhitelist.sh
+++ addWhitelist.sh
@@ -4,7 +4,7 @@
 # 2. Add nameserver's IP addresses 208.67.222.220 and 208.67.222.222.
 # 3. Connect to SSID YSC-SPARK using gosaints as the key.
 # 4. Open Kahn Academy (kahnacademy.org) to confirm.
-sudo sh -c "{ echo nameserver 208.67.222.220 ; echo nameserver 208.67.222.222; }  >> /etc/resolv.conf"
+sudo sh -c "{ echo nameserver 208.67.222.220; echo nameserver 208.67.222.222; }  >> /etc/resolv.conf"
 ifconfig wlan0 up
 iwconfig wlan0 essid YSC-SPARK key s:gosaints
 dhclient wlan0
