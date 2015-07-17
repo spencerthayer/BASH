1bce0e74f07a52ef98bb657a8f30752df6285335
Spencer Thayer <me@spencerthayer.com>
2015-07-17 15:48:51 -0500

    .

---
+++ addWhitelist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# STEPS TO TAKE
+# 1. Write to /etc/resolv.conf using sudo and echo pipping.
+# 2. Add nameserver's 208.67.222.220 and 208.67.222.222.
+# 3. Connect to SSID YSC-SPARK using gosaints as the key.
+# 4. Open Kahn Academy (kahnacademy.org) to confirm.
+sudo sh -c "{ echo nameserver 208.67.222.220 ; echo nameserver 208.67.222.222; }  >> /etc/resolv.conf"
+ifconfig wlan0 up
+iwconfig wlan0 essid YSC-SPARK key s:gosaints
+dhclient wlan0
+echo "CONNECTED TO WIFI!"
+xdg-open http://kahnacademy.org/
+done
