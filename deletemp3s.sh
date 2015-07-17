0a4e2a102b8a075e8b17083c7b2340160bd90f36
Spencer Thayer <me@spencerthayer.com>
2015-07-17 14:29:24 -0500

    .

--- deletemp3s.sh
+++ deletemp3s.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# Loop through unrelated files (*.txt, directories, etc)
-for f in ~/*; do
-    f="${f%.*}"
-    if [[ -f ${f}.mp3 ]]; then
-        rm -f "${f}.mp3" && echo >&2 "${f}.mp3 deleted"
-    fi
-done
-#find ~/ -type f -regextype posix-extended -iregex '.*\.(avi|mkv|wmv|mp4|mp5|flv|M4V|mpeg|mov|m1v|m2v|3gp|avchd)$' -delete
-#
-#for ext in avi mkv wmv mp4 mp5 flv M4V mpeg mov m1v m2v 3gp avchd; do
+# OPTION 1
+# for f in ~/*; do
+#     f="${f%.*}"
+#     if [[ -f ${f}.mp3 ]]; then
+#         rm -f "${f}.mp3" && echo >&2 "${f}.mp3 deleted"
+#     fi
+# done
+# OPTION 2
+find ~/ -type f -regextype posix-extended -iregex '.*\.(wav|wave|mp2|mp3|mp4|mp4a|mp5|m4p|m4b|mpga|3ga|3gp|flac|cda|rec|ra|ram|rmx|rmj|rmm|m4a|m4v|wma|wmv|au|aiff|ogg|oga|midi|d00|avi|mkv|wmv|flv|M4V|mpeg|mov|m1v|m2v|avchd)$' -delete
+# OPTION 3
+# for ext in wav wave mp2 mp3 mp4 mp4a mp5 m4p m4b mpga 3ga 3gp flac cda rec ra ram rmx rmj rmm m4a m4v wma wmv au aiff ogg oga midi d00 avi mkv wmv flv M4V mpeg mov m1v m2v avchd; do
 #  rm -f ~/*.$ext;
-#done
+# done
