0e586d68a21b5546795f6ebd0d4763b2f19d2307
Spencer Thayer <me@spencerthayer.com>
2015-07-17 18:10:26 -0500

    .

--- cleanMedia.sh
+++ cleanMedia.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 find ~/ -type f -regextype posix-extended -iregex '.*\.(wav|wave|mp2|mp3|mp4|mp4a|mp5|m4p|m4b|mpga|3ga|3gp|flac|cda|rec|ra|ram|rmx|rmj|rmm|m4a|m4v|wma|wmv|au|aiff|ogg|oga|midi|d00|avi|mkv|wmv|flv|M4V|mpeg|mov|m1v|m2v|avchd)$' -delete
 done
-# OPTION 2 DO NOT USE
-# for ext in wav wave mp2 mp3 mp4 mp4a mp5 m4p m4b mpga 3ga 3gp flac cda rec ra ram rmx rmj rmm m4a m4v wma wmv au aiff ogg oga midi d00 avi mkv wmv flv M4V mpeg mov m1v m2v avchd; do
-#  rm -f ~/*.$ext;
-# done
-# OPTION 3 DO NOT USE
-# for f in ~/*; do
-#     f="${f%.*}"
-#     if [[ -f ${f}.mp3 ]]; then
-#         rm -f "${f}.mp3" && echo >&2 "${f}.mp3 deleted"
-#     fi
-# done
