abcf6bef1074023512973c826c0539cbba5dfae8
Spencer Thayer <me@spencerthayer.com>
2015-09-10 19:47:17 -0500

    .

--- native_instruments_removal_script.sh
+++ native_instruments_removal_script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# Removing files as indicated, https://www.native-instruments.com/en/support/knowledge-base/show/413/how-to-uninstall-native-instruments-software-and-drivers-from-a-mac-os-x-computer/
-read -p "Are you sure you want to uninstall all Native Instruments software and drivers? " -n 1 -r
-echo # (optional) move to a new line
+# This sceipt removes all Native Instruments files.
+# https://www.native-instruments.com/en/support/knowledge-base/show/413/how-to-uninstall-native-instruments-software-and-drivers-from-a-mac-os-x-computer/
+read -p "Are you sure you want to uninstall all Native Instruments software and drivers?" -n 1 -r
+echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  echo
   echo "Uninstalling..."
   rm -rf /Applications/Native\ Instruments
   rm -f /Library/Preferences/com.native-instruments.*.plist
@@ -14,7 +16,8 @@ then
   rm -rf /Library/Application\ Support/Native\ Instruments
   rm -f ~/Library/Preferences/com.native-instruments.*.plist
   rm -rf ~/Library/Application\ Support/Native\ Instruments
-  rm -f /System/Library/Extensions/NIUSB*.kext
+  rm -rf /System/Library/Extensions/NIUSB*.kext
+  echo
   echo "EXIT"
 fi
 exit
